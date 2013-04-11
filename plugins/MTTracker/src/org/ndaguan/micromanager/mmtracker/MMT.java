abe59bbe64aba6c72bd12d4139f6a7c3749636cb
Erisir <n.daguan@gmail.com>
2013-04-11 22:56:30 +0800

    remove recursive calibration

--- plugins/MTTracker/src/org/ndaguan/micromanager/mmtracker/MMT.java
+++ plugins/MTTracker/src/org/ndaguan/micromanager/mmtracker/MMT.java
@@ -26,36 +26,38 @@ public class MMT {
 		"pixelToPhysY",
 		"precision",
 		"xFactor",
-		"yFactor"};
-	public static String[] VariablesNUPD = new String[]{
+	"yFactor"};
+	public static String [][] VariablesNUPD = new String [][]{
 		//name unit precision,default value,important
-		"beanRadius","/uM","3","1.4","1",
-		"contourLen","/uM","3","1","1",
-		"calRange","/uM","3","2","1",
-		"calStepSize","/uM","3","0.1","1",
-		"rInterStep","/pixel","2","0.5","0",
-		"magnetStepSize","/uM","0","100","1",
-		"frameToCalcForce","/f","0","1000","1",
-		"beanRadiuPixel","/pixel","0","40","1",
-		"persistance","uM","0","0.05","0",
-		"kT","pN/nM","1","4.2","0",
-		"pixelToPhysX","(Um/pixel)","3","0.75","0",
-		"pixelToPhysY","(Um/pixel)","3","0.75","0",
-		"precision","/uM","3","0.001","1",
-		"xFactor","","3","1","0",
-		"yFactor","","3","1","0"
-		};
+
+		{ "beanRadius","/uM" ,"3" ,"1.4" ,"1" },
+		{ "contourLen","/uM" ,"3" ,"1" ,"1" },
+		{ "calRange","/uM" ,"3" ,"2" ,"1" },
+		{ "calStepSize","/uM" ,"3" ,"0.1" ,"1" },
+		{ "rInterStep","/pixel" ,"2" ,"0.5" ,"0" },
+		{ "magnetStepSize","/uM" ,"0" ,"100" ,"1" },
+		{ "frameToCalcForce","/f" ,"0" ,"1000" ,"1" },
+		{ "beanRadiuPixel","/pixel" ,"0" ,"40" ,"1" },
+		{ "persistance","uM" ,"0" ,"0.05" ,"0" },
+		{ "kT","pN/nM" ,"1" ,"4.2" ,"0" },
+		{ "pixelToPhysX","(Um/pixel)" ,"3" ,"0.75" ,"0" },
+		{ "pixelToPhysY","(Um/pixel)" ,"3" ,"0.75" ,"0" },
+		{ "precision","/uM" ,"3" ,"0.001" ,"1" },
+		{ "xFactor","" ,"3" ,"1" ,"0" },
+		{ "yFactor","" ,"3" ,"1" ,"0" }
+	};
+
 	public static String[] UNIT = new String[]{
 		"/uM","/uM","/uM","/uM","/pixel","/uM","",
 		"/pixel","nM","pN/nM","(Um/pixel)","(Um/pixel)","/uM","",""};
 	public static int[] PRECISION = new int[]{
 		3,3,3,3,2,0,0,0,0,1,3,3,3,3,3};
-	
-	
+
+
 	public static String[] CHARTLIST = new String[]{
 		"Chart-Z","Chart-X","Chart-Y","Chart-FX","Chart-FY",
 		"Chart-STDXDY","Chart-SKREWNESS","Chart-Testing","Chart-Corr","Chart-PosProfile"
-		};
+	};
 
 	public static void logError(String string) 
 	{
